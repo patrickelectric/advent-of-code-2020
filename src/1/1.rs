e8a09ff791fc631d01675eaece7a226176628833
Patrick José Pereira <patrickelectric@gmail.com>
2020-12-01 14:23:11 -0300

    1.1: Small performance improvement
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/1/1.rs
+++ src/1/1.rs
@@ -1,18 +1,18 @@
 use aoc2020::helper;
 
 fn main() {
-    let values = helper::get_numbers_from_file("1-input.txt");
+    let mut values = helper::get_numbers_from_file("1-input.txt");
+    values.sort();
 
-    // slow way, but good enough
-    'out_loop: for (i_index, i_value) in values.iter().enumerate() {
-        for (index, value) in values.iter().enumerate() {
-            if index == i_index {
-                continue;
-            }
-
+    'out_loop: for i_value in values.iter() {
+        'inner_loop: for value in values.iter().rev() {
             if value + i_value == 2020 {
                 helper::print_answer("1-1", value * i_value);
                 break 'out_loop;
+            }
+
+            if value + i_value < 2020 {
+                break 'inner_loop;
             }
         }
     }
