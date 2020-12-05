4455c2717a97a7e6d0a127a5927df23500f78768
Patrick José Pereira <patrickelectric@gmail.com>
2020-12-05 12:47:32 -0300

    5: Improve logic approach
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/5/1.rs
+++ src/5/1.rs
@@ -3,15 +3,7 @@ use aoc2020::helper;
 mod ticket;
 
 fn main() {
-    let content = helper::get_input_file("5-input.txt");
-    let mut highest_id = 0;
-    for line in content.lines() {
-        let (row_number, column_number) = ticket::get_seat_from_ticket(line);
-        let id = row_number * 8 + column_number;
-        if id > highest_id {
-            highest_id = id;
-        }
-    }
-
-    helper::print_answer("5-1", highest_id);
+    let ids = ticket::get_tickets_id_from_file("5-input.txt");
+    let highest_id = ids.last().unwrap();
+    helper::print_answer("5-1", *highest_id);
 }
--- src/5/ticket.rs
+++ src/5/ticket.rs
@@ -1,8 +1,23 @@
-pub fn get_seat_from_ticket(ticket: &str) -> (u64, u64) {
-    let (rows, columns) = ticket.split_at(7);
-    let row_number = rows.replace("F", "0").replace("B", "1");
-    let column_number = columns.replace("L", "0").replace("R", "1");
-    let row_number = u64::from_str_radix(&row_number, 2).unwrap();
-    let column_number = u64::from_str_radix(&column_number, 2).unwrap();
-    return (row_number, column_number);
+use aoc2020::helper;
+
+pub fn get_id_from_ticket(ticket: &str) -> u64 {
+    return u64::from_str_radix(
+        &ticket
+            .replace("F", "0")
+            .replace("B", "1")
+            .replace("L", "0")
+            .replace("R", "1"),
+        2,
+    )
+    .unwrap();
+}
+
+pub fn get_tickets_id_from_file(filename: &str) -> Vec<u64> {
+    let content = helper::get_input_file(filename);
+    let mut tickets: Vec<u64> = content
+        .lines()
+        .map(|line| get_id_from_ticket(line))
+        .collect();
+    tickets.sort();
+    return tickets;
 }
