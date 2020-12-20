864e78e86fa44f979597c0bbe5ec6914b1663317
Patrick José Pereira <patrickelectric@gmail.com>
2020-12-20 19:35:05 -0300

    wip
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ src/20/1.rs
@@ -0,0 +1,52 @@
+use aoc2020::helper;
+
+use regex::Regex;
+
+#[derive(Debug)]
+pub struct Tile {
+    pub id: u64,
+    image: String,
+}
+
+impl Tile {
+    pub fn new(tile: &str) -> Self {
+        let re = Regex::new(r"\d+").unwrap();
+        let mut lines = tile.lines();
+        let tile_header = lines.next().unwrap();
+        let id: u64 = re
+            .captures(tile_header)
+            .unwrap()
+            .get(0)
+            .unwrap()
+            .as_str()
+            .parse::<u64>()
+            .unwrap();
+
+        let image = lines.fold(String::new(), |a, b| {
+            if a.is_empty() {
+                b.to_string()
+            } else {
+                a + "\n" + b
+            }
+        });
+
+        return Self { id, image };
+    }
+}
+
+fn main() {
+    let content = helper::get_input_file("20-input.txt");
+    helper::print_answer("20-1", 0);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        let input = include_str!("example.txt");
+        let tiles: Vec<Tile> = input.split("\n\n").map(|tile| Tile::new(tile)).collect();
+        println!("tiles: {:#?}", tiles);
+    }
+}
